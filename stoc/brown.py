8a89705391fff9a28041b3280a8e09ed146c5dad
Yoonku Hwang <yz0624@gmail.com>
2016-09-06 18:23:37 +0900

    add generator brownian motion

--- stoc/brown.py
+++ stoc/brown.py
@@ -1,13 +1,21 @@
 import numpy as np
-def bm(size, seed = None):
+def bm(size, seed = None, B0 = 0, T = 1.):
 	if(seed):
 		np.random.seed(seed)
 
-	val = np.random.randn(size)
-	return sum(val)
+	dt = T / (size - 1)
+	zero = np.zeros(1)
+	val = dt * np.random.randn(size-1)
+	val = np.insert(zero, 1, val)
+	val = np.cumsum(val)
+
+	return val
 
 def sde():
 	return "hello"
 
 def bsde():
 	return "hello"
+
+if __name__ == '__main__':
+	print bm(10)
