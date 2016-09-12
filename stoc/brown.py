697b86ac80392fcb9724eabdb4f9d3a173ef0318
Yoonku Hwang <yz0624@gmail.com>
2016-09-12 14:21:08 +0900

    fix bug

--- stoc/brown.py
+++ stoc/brown.py
@@ -5,7 +5,7 @@ def bm(size, seed = None, B0 = 0, T = 1.):
 
 	dt = T / (size - 1)
 	zero = np.zeros(1)
-	val = dt * np.random.randn(size-1)
+	val = np.sqrt(dt) * np.random.randn(size-1)
 	val = np.insert(zero, 1, val)
 	val = np.cumsum(val)
 
@@ -16,6 +16,3 @@ def sde():
 
 def bsde():
 	return "hello"
-
-if __name__ == '__main__':
-	print bm(10)
